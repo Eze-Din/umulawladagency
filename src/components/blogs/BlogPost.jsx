92a76ff8615c4b4d916663394eafd6ac0abbbe55
sulibassbot <sulibraryassistantbot@gmail.com>
2024-09-20 08:31:56 -0700

    blog added

--- src/components/blogs/BlogPost.jsx
+++ src/components/blogs/BlogPost.jsx
@@ -9,7 +9,9 @@ function BlogPost() {
   return post ? (
     <div className="max-w-2xl mx-auto p-6 bg-gray-600 rounded-lg shadow-lg mt-8">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+      <img src={post.image} alt={post.title} className="w-full h-64 object-cover rounded-t-lg mb-4" />
       <p className="text-base mb-6">{post.content}</p>
+      <p className="text-base mb-6 text-right">{post.date}</p>
       <a href="/en/blog" className="text-blue-500 hover:underline">
         Go Back to Blog
       </a>
